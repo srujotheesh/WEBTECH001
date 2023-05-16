bc32840f8838090d09434d98c38238ce8bbaa69f
srujotheesh <105485964+srujotheesh@users.noreply.github.com>
2023-05-16 18:37:45 +0530

    Add files via upload

---
+++ schema.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    bid: Number,
+    bname: String,
+    pubname: String,
+    category: String,
+    price: Number
+});
+module.exports.Books = mongoose.model("Books",bookSchema);
